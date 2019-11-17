6c541ef0d7dac528e9ba42fe9903822004a6e639
이용현 <gch02310@naver.com>
2019-11-17 13:32:36 +0900

    coroutine android dependency 추가

--- app/src/main/java/com/lyh/dailydialog/presenter/MainActivity.kt
+++ app/src/main/java/com/lyh/dailydialog/presenter/MainActivity.kt
@@ -3,11 +3,14 @@ package com.lyh.dailydialog.presenter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lyh.dailydialog.R
+import com.lyh.dailydialog.data.api.emotion.EmotionRequest
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        EmotionRequest.Builder.setContent("no").build()
     }
 }
