548cffe2e59530b200a81253d199c5bf4da19798
이용현 <gch02310@naver.com>
2019-12-24 01:14:39 +0900

    splash화면 추가

---
+++ app/src/main/java/com/lyh/dailydialog/presenter/splash/SplashFragment.kt
@@ -0,0 +1,47 @@
+package com.lyh.dailydialog.presenter.splash
+
+
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.lyh.dailydialog.R
+import com.lyh.dailydialog.presenter.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_splash.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : BaseFragment(R.layout.fragment_splash) {
+
+    private val splashAnimator = ValueAnimator.ofFloat(0f, 1f)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        splashAnimator.apply {
+            addUpdateListener {
+                splash_logo.scaleX = it.animatedValue as Float
+                splash_logo.scaleY = it.animatedValue as Float
+            }
+
+            doOnEnd {
+                lifecycleScope.launch {
+                    delay(300)
+                    findNavController().navigate(R.id.action_splashFragment_to_recordFragment)
+                }
+            }
+            interpolator = AccelerateInterpolator()
+            duration = 1000
+            start()
+        }
+    }
+
+    fun stopAnimation() {
+        splashAnimator.end()
+    }
+
+}
